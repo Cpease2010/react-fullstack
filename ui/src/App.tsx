8b314e0dad5c03cb5385575ba2ffa7bd62ea5f4f
Cory Pease <cory.pease@galvanize.com>
2022-04-05 10:26:00 -0600

    fetch and display all entities

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -4,40 +4,32 @@ import { client } from './client'
 import './styles/App.css'
 
 const App: FC = () => {
-  const statusMessage = useStatusMessage()
+  const [entities, setEntities] = useState([])
+
+  useEffect(() => {
+    const getEntities = async () => {
+      try {
+        const result = await client.get('/entities')
+        setEntities(result.data)
+      } catch (e) {
+        setEntities([])
+      }
+    }
+    getEntities()
+  }, [])
+
   return (
     <div className='app'>
       <div className='app-banner'>
-        <p style={{ whiteSpace: 'pre' }}>{statusMessage}</p>
+        {entities.map(({ id, name, age }, index) => (
+          <ul key={index}>
+            <li key={id}>{id}</li>
+            <li key={name}>{name}</li>
+            <li key={age}>{age}</li>
+          </ul>
+        ))}
       </div>
     </div>
   )
 }
-
-/**
- * This is a hook to determine whether or not the API is connected.
- * You'll need the API to complete the challenge,
- * so this is a little helper to make sure everything is wired up properly.
- *
- * Once you see that the API is connected properly, you can remove this hook as it's no longer needed. */
-const useStatusMessage = () => {
-  const [isApiConnected, setIsApiConnected] = useState(false)
-  useEffect(() => {
-    const testApiPing = async () => {
-      try {
-        const result = await client.get('/ping')
-        if (result.data === 'pong') setIsApiConnected(true)
-      } catch (e) {
-        setIsApiConnected(false)
-      }
-    }
-    testApiPing()
-  }, [])
-
-  const statusMessage = isApiConnected
-    ? 'The API is connected. Feel free to get started!'
-    : 'The API is not connected. \nPlease follow the instructions in the API folder to start up the server'
-  return statusMessage
-}
-
 export default App
